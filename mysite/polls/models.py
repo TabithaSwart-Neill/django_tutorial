1e4a3fb6718a8505db38489239cf0c5522a784bb
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-08-17 21:08:13 +1000

    fix question model
    fix question model in models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField()
     def __str__(self):
         return self.question_text
-
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(
@@ -22,7 +23,3 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-class Question(models.Model):
-    # ...
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
