e301fb8b5a78b129867f8883dd1066ade59a6c73
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-08-17 17:33:36 +1000

    Tutorial Part2
    editing the Question model (in the polls/models.py file) and adding a __str__() method to both Question and Choice:

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,15 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -14,3 +19,10 @@ class Choice(models.Model):
     )
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
